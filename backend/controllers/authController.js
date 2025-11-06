f2b4672f0fe1bedae31a4680ac9c437102e6c641
vinodravi84 <vinodravi12b2svv@gmail.com>
2025-11-06 19:05:48 +0530

    Login fixed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,14 +14,11 @@ exports.register = async (req, res) => {
     if (existingUser)
       return res.status(400).json({ message: "User already exists" });
 
-    // Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
     // Create user
     const user = await User.create({
       name,
       email,
-      password: hashedPassword,
+      password,
       role: role || "passenger", // default role is passenger
     });
 
